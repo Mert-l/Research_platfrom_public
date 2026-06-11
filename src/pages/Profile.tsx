c0a9b81aae81e3793419d18a0eaeae36be99fb2f
Mert-l <marrta.lm@gmail.com>
2026-06-11 17:42:53 +0200

    yet another fix

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -649,7 +649,7 @@ export default function Profile() {
               <div className="max-h-[400px] overflow-y-auto space-y-3 text-left text-sm">
                 <p>
                   This application is used as part of a research project studying
-                  parrot interaction with a sound-playing device. As a user, you agree that the name of the files (not the actual files) you upload and the personal information of your petcan be seen by the researcher.
+                  parrot interaction with a sound-playing device. As a user, you agree that the name of the files (not the actual files) you upload and the personal information of your pet can be seen by the researcher.
                 </p>
               </div>
             </AlertDialogDescription>
